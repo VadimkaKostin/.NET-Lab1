f10235ee2add9ebf1fb0143a9c449c914d253baf
VadimKostin <vadkostinwork@gmail.com>
2023-09-13 15:25:51 +0300

    Add custom collection class

---
+++ Lab1/Lab1/MyArray.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class MyArray<T> : ICollection<T>
+    {
+        private int _count;
+        private int _capacity;
+        private T[] _items;
+
+        public int Count => _count;
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public MyArray()
+        {
+            _items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
